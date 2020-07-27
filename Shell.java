4e6e8ddb61ec267f5450365ad641f4c2d5a203ac
Julien Descannevelle <jdesca.eirl@gmail.com>
2020-07-27 13:40:06 +0000

    commit

--- Shell.java
+++ Shell.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.manchabalais.javareverseshell;
-
 /**
  *
- * @author kali
+ * @author Julien Descannevelle
  */
 public class Shell {
 
@@ -35,3 +28,4 @@ public class Shell {
     }
 
 }
+
